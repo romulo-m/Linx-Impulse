d24c04ddb8236f3592e7ca5b1f47bf69bfee0c10
Broad <romulo.mgaudencio@gmail.com>
2022-08-18 14:39:51 -0300

    todo-react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './assets/components/Header/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
