15cf38115032b3eec8ac7eaa648907292e489069
Broad <romulo.mgaudencio@gmail.com>
2022-08-20 17:01:27 -0300

    componente footer finalizado

---
+++ src/assets/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <p className={style.p}>Testando suas habilidades em HTML, CSS e JS.</p>
+      <p className={style.p}>Linx Impulse</p>
+      <p className={style.p}>2019</p>
+    </footer>
+  )
+}
+
+export default Footer
