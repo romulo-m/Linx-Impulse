4da54d467b74cdb31e645d951e58a80690aacc8e
Broad <romulo.mgaudencio@gmail.com>
2022-08-21 18:23:31 -0300

    implementação do footer no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './assets/components/Footer/Footer'
 import Header from './assets/components/Header/Header'
 import Main from './assets/components/Main/Main'
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
